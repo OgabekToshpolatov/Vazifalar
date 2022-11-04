7a9c2af0638ee3b27d832d5c204f5e7afb79523b
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-11-04 23:33:12 +0500

    Delete All

--- claim/Program.cs
+++ claim/Program.cs
@@ -1,13 +1,11 @@
-using claim.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<JsonService>();
 
+ 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
