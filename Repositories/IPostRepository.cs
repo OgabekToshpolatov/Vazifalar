2e9e00f128a89a3ae6a1a80803d8fa3ea149c948
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-12-11 02:05:29 +0500

    All Change Projects

---
+++ Repositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using BlogMvc.Entities;
+
+namespace BlogMvc.Repositories;
+
+public interface IPostRepository
+{
+    Task<Post> CreateAsync(Post post, string userId);
+    Task<Post> GetPostAsync(long id);
+    Task<IEnumerable<Post>> GetPostsAsync(Expression<Func<Post, bool>> exp);
+    Task<Post> UpdateAsync(Post post);
+    Task DeleteAsync(long id);
+}
